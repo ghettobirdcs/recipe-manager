366d2b046aeae0d55bc89d7589b7207b7f9ca0ac
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-08-25 18:55:39 -0600

    Responsive RecipeList

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from "react";
-import MobileModal from "../UI/MobileModal";
+import React from "react";
 import "./RecipeCard.scss";
 
 const RecipeCard = ({ recipe, onView, onEdit, onDelete, onFavorite }) => {
-  const [isMobileOpen, setIsMobileOpen] = useState(false);
-
   return (
     <div className="recipe__card">
       <div className="recipe__title">{recipe.title}</div>
@@ -12,11 +9,6 @@ const RecipeCard = ({ recipe, onView, onEdit, onDelete, onFavorite }) => {
         Ingredients:{" "}
         <span className="recipe__ingredients">{recipe.ingredients}</span>
       </p>
-      <MobileModal
-        recipe={recipe}
-        setIsMobileOpen={setIsMobileOpen}
-        isMobileOpen={isMobileOpen}
-      />
       <div className="recipe__actions">
         <button onClick={() => onView && onView(recipe)}>View</button>
         <button onClick={() => onEdit && onEdit(recipe)}>Edit</button>
